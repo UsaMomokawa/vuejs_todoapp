816634e9ad810572a18130175bae0f98005cb0a2
UsaMomokawa <spcmomo@gmail.com>
2019-05-02 16:26:20 +0900

    lesson3の課題を追加

---
+++ lesson3/v-for-array2.js
@@ -0,0 +1,10 @@
+const app = new Vue({
+  el: '#app',
+  data: {
+    pages: [
+      { id: 1, work_id: 1, status: "not_start" },
+      { id: 2, work_id: 1, status: "not_start" },
+      { id: 3, work_id: 1, status: "not_start" }
+    ]
+  }
+})
